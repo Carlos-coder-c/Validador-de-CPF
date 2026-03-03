c62c444eab925781fa0adb1865c856aeb17a12a2
Carlos Eduardo <menino.podsonhar@gmail.com>
2026-03-03 08:52:15 -0300

    Adicionando o codigo do dígito 2

--- validador.py
+++ validador.py
@@ -14,4 +14,19 @@ for digito_1 in cpf_nove_digitos:
 
 digito_1 = (resultado * 10) % 11
 digito_1 = digito_1  if digito_1 <= 9 else 0
-print(digito_1)
+
+
+
+cpf_dez_digitos = cpf_nove_digitos + str(digito_1)
+
+contagem_11 = 11
+resultado_2 = 0
+
+for digito_2 in cpf_dez_digitos:
+    resultado_2 += int(digito_2) * contagem_11
+    contagem_11 -= 1
+
+digito_2 = (resultado_2 * 10) % 11
+digito_2 = digito_2 if digito_2 <= 9 else 0
+
+print(digito_1, digito_2)
